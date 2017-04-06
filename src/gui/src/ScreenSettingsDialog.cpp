db58863cd54ec70e9f2a1547e24c1ad679674e2a
yupi2 <yupi2@openmailbox.org>
2017-04-06 02:03:39 +0000

    Allow spaces in Client Names through the Qt Gui

--- src/gui/src/ScreenSettingsDialog.cpp
+++ src/gui/src/ScreenSettingsDialog.cpp
@@ -2,11 +2,11 @@
  * synergy -- mouse and keyboard sharing utility
  * Copyright (C) 2012-2016 Symless Ltd.
  * Copyright (C) 2008 Volker Lanz (vl@fidra.de)
- * 
+ *
  * This package is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * found in the file LICENSE that should have accompanied this file.
- * 
+ *
  * This package is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -30,7 +30,7 @@ ScreenSettingsDialog::ScreenSettingsDialog(QWidget* parent, Screen* pScreen) :
 {
 	setupUi(this);
 
-	QRegExp validScreenName("[a-z0-9\\._-]{,255}", Qt::CaseInsensitive);
+	QRegExp validScreenName("[a-z0-9\\._- ]{,255}", Qt::CaseInsensitive);
 
 	m_pLineEditName->setText(m_pScreen->name());
 	m_pLineEditName->setValidator(new QRegExpValidator(validScreenName, m_pLineEditName));
