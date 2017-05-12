c05e0047500b166f632dcd7a185fa264fbb73243
Andrew Nelless <andrew@symless.com>
2017-05-12 17:12:44 +0000

    #6037 Replaced empty()->clear() mixup

--- src/lib/ipc/IpcServer.cpp
+++ src/lib/ipc/IpcServer.cpp
@@ -2,11 +2,11 @@
  * synergy -- mouse and keyboard sharing utility
  * Copyright (C) 2012-2016 Symless Ltd.
  * Copyright (C) 2012 Nick Bolton
- * 
+ *
  * This package is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * found in the file LICENSE that should have accompanied this file.
- * 
+ *
  * This package is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -80,10 +80,10 @@ IpcServer::~IpcServer()
 	for (it = m_clients.begin(); it != m_clients.end(); it++) {
 		deleteClient(*it);
 	}
-	m_clients.empty();
+	m_clients.clear();
 	ARCH->unlockMutex(m_clientsMutex);
 	ARCH->closeMutex(m_clientsMutex);
-	
+
 	m_events->removeHandler(m_events->forIListenSocket().connecting(), m_socket);
 }
 
