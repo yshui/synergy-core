ae53fe381c083b2f5a8e89d3cf153dc19037fa81
Andrew Nelless <andrew@symless.com>
2017-05-12 17:18:20 +0000

    #6037 Remove crazy LoadString wrapper

--- src/lib/platform/MSWindowsUtil.cpp
+++ src/lib/platform/MSWindowsUtil.cpp
@@ -2,11 +2,11 @@
  * synergy -- mouse and keyboard sharing utility
  * Copyright (C) 2012-2016 Symless Ltd.
  * Copyright (C) 2004 Chris Schoeneman
- * 
+ *
  * This package is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * found in the file LICENSE that should have accompanied this file.
- * 
+ *
  * This package is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -29,31 +29,14 @@
 String
 MSWindowsUtil::getString(HINSTANCE instance, DWORD id)
 {
-	char buffer[1024];
-	int size = static_cast<int>(sizeof(buffer) / sizeof(buffer[0]));
-	char* msg = buffer;
+	char* msg = NULL;
+	int n = LoadStringA(instance, id, reinterpret_cast<LPSTR>(&msg), 0);
 
-	// load string
-	int n = LoadString(instance, id, msg, size);
-	msg[n] = '\0';
-	if (n < size) {
-		return msg;
+	if (n <= 0) {
+		return String();
 	}
 
-	// not enough buffer space.  keep trying larger buffers until
-	// we get the whole string.
-	msg = NULL;
-	do {
-		size <<= 1;
-		delete[] msg;
-		char* msg = new char[size];
-		n = LoadString(instance, id, msg, size);
-	} while (n == size);
-	msg[n] = '\0';
-
-	String result(msg);
-	delete[] msg;
-	return result;
+	return String (msg, n);
 }
 
 String
