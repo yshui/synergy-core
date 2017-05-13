0de526b93441ff8c7a89bd3ec7f1289fa9c7294b
Andrew Nelless <andrew@symless.com>
2017-05-13 03:10:00 +0000

    #6037 Use MAKEINTATOM macro in place of dodgy casts

--- src/lib/platform/MSWindowsDesks.cpp
+++ src/lib/platform/MSWindowsDesks.cpp
@@ -458,7 +458,7 @@ void
 MSWindowsDesks::destroyClass(ATOM windowClass) const
 {
 	if (windowClass != 0) {
-		UnregisterClass(reinterpret_cast<LPCTSTR>(windowClass),
+		UnregisterClass(MAKEINTATOM(windowClass),
 							MSWindowsScreen::getWindowInstance());
 	}
 }
@@ -468,7 +468,7 @@ MSWindowsDesks::createWindow(ATOM windowClass, const char* name) const
 {
 	HWND window = CreateWindowEx(WS_EX_TRANSPARENT |
 									WS_EX_TOOLWINDOW,
-								reinterpret_cast<LPCTSTR>(windowClass),
+								MAKEINTATOM(windowClass),
 								name,
 								WS_POPUP,
 								0, 0, 1, 1,
