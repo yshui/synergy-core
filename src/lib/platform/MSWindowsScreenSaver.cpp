73f421565351e34b41c7daa1d0377593aeca6f26
Andrew Nelless <andrew@symless.com>
2017-05-20 18:45:11 +0000

    #6037 Unitialized members in MSWindowsScreenSaver

--- src/lib/platform/MSWindowsScreenSaver.cpp
+++ src/lib/platform/MSWindowsScreenSaver.cpp
@@ -2,11 +2,11 @@
  * synergy -- mouse and keyboard sharing utility
  * Copyright (C) 2012-2016 Symless Ltd.
  * Copyright (C) 2002 Chris Schoeneman
- * 
+ *
  * This package is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * found in the file LICENSE that should have accompanied this file.
- * 
+ *
  * This package is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -50,6 +50,9 @@ MSWindowsScreenSaver::MSWindowsScreenSaver() :
 	m_process(NULL),
 	m_watch(NULL),
 	m_threadID(0),
+	m_msg(0),
+	m_wParam(0),
+	m_lParam(0),
 	m_active(false)
 {
 	// check if screen saver is enabled
