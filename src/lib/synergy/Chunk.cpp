0bbb99a751bd85b89c1472dc5877c459aa6e6cec
Andrew Nelless <andrew@symless.com>
2017-05-20 18:53:58 +0000

    #6037 Unitialized member in Chunk

--- src/lib/synergy/Chunk.cpp
+++ src/lib/synergy/Chunk.cpp
@@ -1,11 +1,11 @@
 /*
  * synergy -- mouse and keyboard sharing utility
  * Copyright (C) 2015-2016 Symless Ltd.
- * 
+ *
  * This package is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * found in the file LICENSE that should have accompanied this file.
- * 
+ *
  * This package is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -18,7 +18,7 @@
 #include "synergy/Chunk.h"
 #include "base/String.h"
 
-Chunk::Chunk(size_t size)
+Chunk::Chunk(size_t size) : m_dataSize(0)
 {
 	m_chunk = new char[size];
 	memset(m_chunk, 0, size);
